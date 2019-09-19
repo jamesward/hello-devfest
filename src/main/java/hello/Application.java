b896680deaaf355513dda16703da74e4a0db4e61
James Ward <james@jamesward.com>
2019-09-19 15:29:08 -0600

    init

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @RequestMapping("/")
+  public String index() {
+    String name = System.getenv("NAME");
+    if (name == null) {
+      name = "world";
+    }
+
+    return String.format("hello, %s", name);
+  }
+
+}
+
